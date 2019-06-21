8732aacf09b338c461dad57abf7933538858844a
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-21 13:46:54 +0200

    recipe and steps interfices

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -10,3 +10,20 @@ export class RecipesService {
    }
 }
 
+// Recipe interface
+export interface Recipe {
+  id: string; // recipe id
+  title: string; // Title
+  img: string; // Main recipe img
+  desc: string; // Recipe description
+  time: string; // Cooking time
+  steps: Step[];
+}
+
+// Steps interface
+export interface Step {
+  number: number;  // step number
+  title: string; // Title
+  img: string; // step img
+  desc: string; // Steps description
+}
