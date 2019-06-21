597dfb6bd61623855fcdcbdfe4fe6a179f167994
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-21 13:56:38 +0200

    paella pollo is coming 🥘

--- src/app/services/recipes.ts
+++ src/app/services/recipes.ts
@@ -1,1 +1,23 @@
+import { Recipe } from './recipes.service';
 // Here will go the recipes info
+export recipes: Recipe[] = [
+{
+    id: 'paella-pollo', 
+    title: 'Paella de Pollo', 
+    img: string; // Main recipe img
+    desc: string; // Recipe description
+    time: string; // Cooking time
+    steps: Step[{
+
+    }];
+}
+
+
+number: number;  // step number
+title: string; // Title
+img: string; // step img
+desc: string; // Steps description
+
+
+
+];
