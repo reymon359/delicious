7760f3f1ee734f0f00bee8a2b700347c16a81482
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-20 12:41:42 +0200

    en.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
 
 // Components
 import { AppComponent } from './app.component';
@@ -11,14 +12,22 @@ import { RecipeComponent } from './components/recipe/recipe.component';
 import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
-// dates in other language
+// Translation
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+// Dates in other language
 import { LOCALE_ID } from '@angular/core';
 import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
 
-// Translation
-import {TranslateModule} from '@ngx-translate/core';
 registerLocaleData(localeEs);
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +41,14 @@ registerLocaleData(localeEs);
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    TranslateModule.forRoot()
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    })
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
