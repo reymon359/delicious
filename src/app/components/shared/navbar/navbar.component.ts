dd333ed589d186c7779c836136306d38b0f99799
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-20 13:35:57 +0200

    language select on navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translate: TranslateService) { }
 
   ngOnInit() {
   }
