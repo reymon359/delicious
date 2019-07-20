83f12809a24520ff5e30c15b0e05bb89397b73e4
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-20 13:42:57 +0200

    navbar hidden on 404

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,19 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(public translate: TranslateService) { }
+  public isNotFound: boolean;
+
+  constructor(public translate: TranslateService,
+              private router: Router) {
+    // Hiding navbar in not found page
+    this.router.events.subscribe(event => {
+      if (event.constructor.name === 'NavigationEnd') {
+        this.isNotFound = (<any>event).url.split('/').slice(-1)[0] === 'not-found';
+      }
+    });
+  }
 
   ngOnInit() {
-  }
+      }
 
 }
