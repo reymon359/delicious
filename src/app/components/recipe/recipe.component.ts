01bf4dd4e4ac3579c3de90161196f608bafa1b0c
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-08-07 07:56:28 +0200

    trying a library

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -35,7 +35,7 @@ export class RecipeComponent implements OnInit {
           this.route.navigate(['not-found']);
         } else {
           this.recipe = recipe;
-          console.log(this.recipe);
+          // console.log(this.recipe);
         }
 
 
@@ -45,63 +45,71 @@ export class RecipeComponent implements OnInit {
 
   ngOnInit() {
     this.location = window.location.href;
+    // this.lazyLoadingImg();
   }
   print() {
     window.print();
   }
 
 
-/**
- * lazyLoadingImgBg()
- * Its adds an Intersection observer to all the elements with
- * backgroundimages that have the class lazy on them.
- */
-lazyLoadingImgBg() {
-  let lazyloadImages;
+  /**
+   * lazyLoadingImg()
+   * Its adds an Intersection observer to all the imgs with
+   * that have the class lazy on them.
+   */
 
-  if ('IntersectionObserver' in window) {
-      lazyloadImages = document.querySelectorAll('.lazy');
-      const imageObserver = new IntersectionObserver((entries, observer) => {
-          entries.forEach((entry) => {
-              if (entry.isIntersecting) {
-                  const image = entry.target;
-                  image.classList.remove('lazy');
-                  imageObserver.unobserve(image);
-              }
-          });
-      });
+  // lazyLoadingImg() {
+  //   console.log('lazy loading')
 
-      lazyloadImages.forEach(function(image) {
-          imageObserver.observe(image);
-      });
-  } else { // Browsers without IntersectionObserver
+  //   let lazyloadImages;
 
-      let lazyloadThrottleTimeout;
-      lazyloadImages = document.querySelectorAll('.lazy');
+  //   if ('IntersectionObserver' in window) {
+  //     lazyloadImages = document.querySelectorAll('.lazy');
+  //     const imageObserver = new IntersectionObserver((entries, observer) => {
+  //       entries.forEach(entry => {
+  //         if (entry.isIntersecting) {
+  //           const image = entry.target;
+  //           //@ts-ignore
+  //           image.src = image.dataset.src;
+  //           image.classList.remove('lazy');
+  //           imageObserver.unobserve(image);
+  //         }
+  //       });
+  //     });
 
-      function lazyload() {
-          if (lazyloadThrottleTimeout) clearTimeout(lazyloadThrottleTimeout);
+  //     lazyloadImages.forEach(image => {
+  //       imageObserver.observe(image);
+  //     });
+  //   } else {
+  //     let lazyloadThrottleTimeout;
+  //     lazyloadImages = document.querySelectorAll('.lazy');
 
-          lazyloadThrottleTimeout = setTimeout(() => {
-              const scrollTop = window.pageYOffset;
-              lazyloadImages.forEach((img) => {
-                  if (img.offsetTop < (window.innerHeight + scrollTop)) {
-                      img.src = img.dataset.src;
-                      img.classList.remove('lazy');
-                  }
-              });
-              if (lazyloadImages.length === 0) {
-                  document.removeEventListener('scroll', lazyload);
-                  window.removeEventListener('resize', lazyload);
-                  window.removeEventListener('orientationChange', lazyload);
-              }
-          }, 20);
-      }
+  //     function lazyload() {
+  //       if (lazyloadThrottleTimeout) {
+  //         clearTimeout(lazyloadThrottleTimeout);
+  //       }
 
-      document.addEventListener('scroll', lazyload);
-      window.addEventListener('resize', lazyload);
-      window.addEventListener('orientationChange', lazyload);
-  }
-}
+  //       lazyloadThrottleTimeout = setTimeout(() => {
+  //         const scrollTop = window.pageYOffset;
+  //         lazyloadImages.forEach(img => {
+  //           if (img.offsetTop < (window.innerHeight + scrollTop)) {
+  //             img.src = img.dataset.src;
+  //             img.classList.remove('lazy');
+  //           }
+  //         });
+  //         if (lazyloadImages.length === 0) {
+  //           document.removeEventListener('scroll', lazyload);
+  //           window.removeEventListener('resize', lazyload);
+  //           window.removeEventListener('orientationChange', lazyload);
+  //         }
+  //       }, 20);
+  //     }
+
+  //     document.addEventListener('scroll', lazyload);
+  //     window.addEventListener('resize', lazyload);
+  //     window.addEventListener('orientationChange', lazyload);
+  //   }
+
+  // }
 
 }
