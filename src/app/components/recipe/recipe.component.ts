c2a270ab8731e35b6c96d7ecb83118034fe1c090
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-08-07 07:25:25 +0200

    adding lazy loading

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -50,4 +50,58 @@ export class RecipeComponent implements OnInit {
     window.print();
   }
 
+
+/**
+ * lazyLoadingImgBg()
+ * Its adds an Intersection observer to all the elements with
+ * backgroundimages that have the class lazy on them.
+ */
+lazyLoadingImgBg() {
+  let lazyloadImages;
+
+  if ('IntersectionObserver' in window) {
+      lazyloadImages = document.querySelectorAll('.lazy');
+      const imageObserver = new IntersectionObserver((entries, observer) => {
+          entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                  const image = entry.target;
+                  image.classList.remove('lazy');
+                  imageObserver.unobserve(image);
+              }
+          });
+      });
+
+      lazyloadImages.forEach(function(image) {
+          imageObserver.observe(image);
+      });
+  } else { // Browsers without IntersectionObserver
+
+      let lazyloadThrottleTimeout;
+      lazyloadImages = document.querySelectorAll('.lazy');
+
+      function lazyload() {
+          if (lazyloadThrottleTimeout) clearTimeout(lazyloadThrottleTimeout);
+
+          lazyloadThrottleTimeout = setTimeout(() => {
+              const scrollTop = window.pageYOffset;
+              lazyloadImages.forEach((img) => {
+                  if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                      img.src = img.dataset.src;
+                      img.classList.remove('lazy');
+                  }
+              });
+              if (lazyloadImages.length === 0) {
+                  document.removeEventListener('scroll', lazyload);
+                  window.removeEventListener('resize', lazyload);
+                  window.removeEventListener('orientationChange', lazyload);
+              }
+          }, 20);
+      }
+
+      document.addEventListener('scroll', lazyload);
+      window.addEventListener('resize', lazyload);
+      window.addEventListener('orientationChange', lazyload);
+  }
+}
+
 }
