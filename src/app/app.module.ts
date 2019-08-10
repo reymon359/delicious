0ba1526b0d4e2a11c8405cf4e2a69cc88bfb9d2d
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-08-10 20:12:08 +0200

    Ready for build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ registerLocaleData(localeEs);
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
